27f09c15b3d5c2c17225782f69bafde4218bc871
MrYKenz <yasin.kenz@kcl.ac.uk>
2020-06-20 13:20:21 +0100

    holdings info w/ cards & sellout topup buttons

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ View Webpage: [Click Here](http://localhost:3000)
     - blockets.json and holdings.json.
 - I have used Google's popular Open Source Material UI component library for some of the input fields (installed via npm).
 - Some styles adopted from Material UI and others are completely custom (e.g. log out button) to match the specfication's font and pallet.
+
+### Changes or Improvements
+- Change avatar to company logos or icons
+- Could use Typescript if preferred & time frame permits
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,48 @@
 import React from 'react'
+// material ui imports
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
 
 function Display({data}) {
+    const sellOut = () => {
+        // sellout functionality
+        console.log("sell out")
+    }
+    const topUp = () => {
+        // topup functionality
+        console.log("top up")
+    }
     return (
         <div>
-            Display
+            <h3 style={{textAlign: 'left', paddingLeft: '1rem', color: '#444'}}>
+                Holdings Information
+            </h3>
+            {data.map((holding, key) => (
+                <Card key={key} style={{margin: '10px'}}>
+                    <CardHeader
+                    avatar={
+                    <Avatar aria-label="recipe" style={{backgroundColor: '#aaa'}}>
+                        {holding.bond[0]}
+                    </Avatar>
+                    }
+                    title={holding.bond}
+                    subheader={holding.size + ' ᛫ £' + holding.price}
+                    action={
+                        <div className='buttons'>
+                            <button style={{marginTop: '6px', marginBottom: '4px'}}
+                            onClick={() => sellOut()}>
+                                Sell Out
+                            </button>
+                            <br/>
+                            <button onClick={() => topUp()}>
+                                Top Up
+                            </button>
+                        </div>
+                    }
+                    />
+                </Card>
+            ))}
         </div>
     )
 }
